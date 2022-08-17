615af14cb450e7b921c6544dd5043bcdda0e97de
Kay Bi <kaybi9x@gmail.com>
2022-08-17 11:02:36 +0700

    update

---
+++ BTQuanLyDonHang.sql
@@ -0,0 +1,30 @@
+create database QuanLyBanHang;
+use QuanLyBanHang;
+
+create table customer (
+	cid int(3) primary key auto_increment,
+    name varchar(20),
+    age int(3)
+);
+
+create table orders (
+	oid int(3) primary key auto_increment,
+    cid int(3),
+    date datetime,
+    totalprice int(10),
+    foreign key (cid) references customer(cid)
+);
+
+create table ordersdetail (
+	oid int(3),
+    pid int(3),
+    qty int(10),
+    foreign key (oid) references orders(oid),
+    foreign key (pid) references product(pid)
+);
+
+create table product (
+	pid int(3) primary key auto_increment,
+    name varchar(10),
+    price int(10)
+);
